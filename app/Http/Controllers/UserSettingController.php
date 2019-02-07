f3c3f12ed6a17b4abd4b2620e5f6b4029d3f4c92
Maratunsolihah29 <maratunsolihah2907@gmail.com>
2019-02-07 07:59:10 +0700

    laravel tokap eps-8

--- app/Http/Controllers/UserSettingController.php
+++ app/Http/Controllers/UserSettingController.php
@@ -9,12 +9,21 @@ use Auth;
 class UserSettingController extends Controller
 {
  	public function form()
- 	   {
- 	   	$data = User::where('id',Auth::id())->first();
- 	   	return view('admin.pages.user.setting',['dt'=>$data]);
- 	   } 
- 	   public function update()
- 	     {
- 	     	return "Fungsi Update";
- 	     }  
+ 	{
+ 		$data = User::where('id',Auth::id())->first();
+ 		return view('admin.pages.user.setting',['dt'=>$data]);
+ 	} 
+
+ 	public function update(Request $req)
+ 	{
+ 	    $id = Auth::id();
+     	\Validator::make($req->all(), [
+ 	     		'name'=>'required|between:3,100',
+ 	     		'email'=>'required|email|unique:users,email,'.$id,
+ 	     		'password'=>'nullable|min:6',
+ 	     		'repassword'=>'same:password',
+ 	   		 ])->validate();
+ 	     	
+ 	    return "Fungsi Update";
+ 	}  
 }
